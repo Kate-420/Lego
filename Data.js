bd0c7f9351b8d7d860dc4a3a31bb343227aa6c08
Cathrine Muigai <catemuigai4@gmail.com>
2023-05-31 14:49:28 +0300

    deleted files

--- Data.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Skills from "C:\Users\Dell\Desktop\My Portfolio\src\components\Skills.js";
-import Testimonials from "C:\Users\Dell\Desktop\My Portfolio\src\components\Testimonial.js;"
-export default function data(){
-    return(
-    <><Testimonials />
-    <Skills /></>
-    );
-}
